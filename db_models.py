2b4a157b444ee89d92d33f60fab4943768726976
Jorj Xenos <JorjXenos@gmail.com>
2023-01-07 11:53:23 -0500

    create db_models file

---
+++ db_models.py
@@ -0,0 +1,7 @@
+# db_models.py
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
