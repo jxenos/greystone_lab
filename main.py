59e5a7bbfae3608c129e866305698ca7cc42c6f6
Jorj Xenos <JorjXenos@gmail.com>
2023-01-07 11:11:29 -0500

    create class for user and create user endpoint

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+MAJOR = 'v1'
+
+
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone: str | None = None
 
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+
+@app.post(f'/{MAJOR}/user/')
+async def create_user(user: User):
+    return user
