427d6e90072ba794149b347e9bbe41a3c7e94e93
Jorj Xenos <JorjXenos@gmail.com>
2023-01-07 12:00:42 -0500

    define user table

--- db_models.py
+++ db_models.py
@@ -5,3 +5,17 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
+
+
+class User_Table(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    # in a real env recommend having a guid as well
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    email = Column(String(50))
+    phone = Column(Integer(10))
+
+    def __repr__(self):
+        return f"User_Table(id={self.id!r}, first={self.first_name!r}, last={self.last_name!r})"
