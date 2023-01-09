9ee9a1ef66bd6610020358c4d448affdff8a74b1
Jorj Xenos <JorjXenos@gmail.com>
2023-01-09 12:07:26 -0500

    stub out endpoint actions

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from db_models import Session
+from db_models import Loan_Table
+from db_models import User_Table
+from db_models import engine
+
+
 app = FastAPI()
 MAJOR = 'v1'
 
@@ -19,6 +25,9 @@ class Loan(BaseModel):
     apr: float
 
 
+session = Session(bind=engine)
+
+
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
@@ -26,11 +35,18 @@ async def root():
 
 @app.post(f'/{MAJOR}/user/')
 async def create_user(user: User):
+    new_user = User_Table(email='', first='', last='', phone='')
+    session.add(new_user)
+    session.commit()
     return user
 
 
 @app.post(f'/{MAJOR}/loan/')
 async def create_loan(loan: Loan):
+    new_loan = Loan_Table(user_id='', principal='',
+                          term='', interest='', start_date='')
+    session.add(new_loan)
+    session.commit()
     return loan
 
 
@@ -40,12 +56,13 @@ async def get_loan_schedule(loan_id):
 
 
 @app.get(f'/{MAJOR}/loan/month')
-async def get_loan_summary(month, year):
-    return month, year
+async def get_loan_summary(month):
+    return month
 
 
 @app.get(f'/{MAJOR}/user/loans')
 async def get_all_user_loans(user_id):
+    session.query(Loan_Table).get(user_id)
     return user_id
 
 
